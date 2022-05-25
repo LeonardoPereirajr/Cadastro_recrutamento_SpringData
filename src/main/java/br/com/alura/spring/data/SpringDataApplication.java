443749dc57a1384527e913eb9de2d974c5499c0b
Leonardo Sanches <leonardopereirajr@gmail.com>
2022-05-25 18:54:50 -0300

    Pesquisas dinamicas

--- src/main/java/br/com/alura/spring/data/SpringDataApplication.java
+++ src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -10,6 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import br.com.alura.spring.data.service.CrudCargoService;
 import br.com.alura.spring.data.service.CrudFuncionarioService;
 import br.com.alura.spring.data.service.CrudUnidadeTrabalhoService;
+import br.com.alura.spring.data.service.RelatorioFuncionarioDinamico;
 import br.com.alura.spring.data.service.RelatoriosService;
 
 @EnableJpaRepositories
@@ -22,15 +23,18 @@ public class SpringDataApplication implements CommandLineRunner {
 	private final CrudFuncionarioService funcionarioService;
 	private final CrudUnidadeTrabalhoService unidadeTrabalhoService;
 	private final RelatoriosService relatoriosService;
+	private final RelatorioFuncionarioDinamico relatorioFuncionarioDinamico;
 	
 	public SpringDataApplication(CrudCargoService cargoService, 
 			CrudFuncionarioService funcionarioService, 
 			CrudUnidadeTrabalhoService unidadeTrabalhoService,
-			RelatoriosService relatoriosService) {
+			RelatoriosService relatoriosService,
+			RelatorioFuncionarioDinamico relatorioFuncionarioDinamico) {
 		this.cargoService = cargoService;
 		this.funcionarioService = funcionarioService;
 		this.unidadeTrabalhoService = unidadeTrabalhoService;
 		this.relatoriosService = relatoriosService;
+		this.relatorioFuncionarioDinamico = relatorioFuncionarioDinamico;
 	}
 
 	public static void main(String[] args) {
@@ -66,9 +70,9 @@ public class SpringDataApplication implements CommandLineRunner {
 				case 4:
 					relatoriosService.inicial(scanner);
 					break;
-//				case 5:
-//					relatorioFuncionarioDinamico.inicial(scanner);
-//					break;
+				case 5:
+					relatorioFuncionarioDinamico.inicial(scanner);
+					break;
 				default:
 					System.out.println("Finalizando");
 					system = false;
